65a998ecca5a6d3edca65fc5318b201702585891
Divyansh Shekhar Gaur <divyanshshekhar@users.noreply.github.com>
2022-05-04 22:35:39 +0530

    Added login form.

--- saml-sp-node/src/app.js
+++ saml-sp-node/src/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Configure passport
+require('./config/passport-config')
+
 var app = express();
 
 // view engine setup
--- saml-sp-node/src/routes/users.js
+++ saml-sp-node/src/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session')
+var passport = require('passport')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post(
+    '/login',
+    passport.authenticate('password', {failureRedirect: '/login', failureMessage: true}),
+    function (req, res) {
+      res.redirect('/');
+    });
+
+router.post(
+    '/logout',
+    function (req, res) {
+      req.logout();
+      res.redirect('/');
+    });
 
 module.exports = router;
